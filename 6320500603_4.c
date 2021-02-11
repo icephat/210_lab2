9c02df30043bdefb47066d12f65c95d7bd22fe6b
icephat <phattaraporn.sa@ku.th>
2021-02-11 17:16:00 +0700

    คิดโค้ดใหม่

--- 6320500603_4.c
+++ 6320500603_4.c
@@ -9,33 +9,57 @@ int main()
     {
         if(b%2!=0)
         {
-            printf("{@_@}");
+            if(b==5||b%10==5)
+            {
+                printf("\(^_^)/");
+            }
+            else
+            {
+                printf("(^_^)");
+            }
+
         }
         else
         {
-            printf("(^_^)");
+            printf("{@_@}");
         }
     }
     else if(a>=74&&a<=82)
     {
-        if(b%2!=0)
+          if(b%2!=0)
         {
-            printf("{@_@}");
+            if(b==5||b%10==5)
+            {
+                printf("\(*o*)/");
+            }
+            else
+            {
+                printf("(*o*)");
+            }
+
         }
         else
         {
-            printf("(^_^)");
+            printf("{*v*}");
         }
     }
     else if(a>=83&&a<=90)
     {
         if(b%2!=0)
         {
-            printf("{@_@}");
+            if(b==5||b%10==5)
+            {
+                printf("\(T_T)/");
+            }
+            else
+            {
+                printf("(T_T)");
+            }
+
         }
         else
         {
-            printf("(^_^)");
+            printf("{x_x}");
         }
     }
 
--- 6320500603_6.c
+++ 6320500603_6.c
@@ -1,221 +1,28 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c,d,e,f;
-
-    scanf("%d",&a);
-    b=a/10000;
-    c=a%10000/1000;
-    d=a%10000%1000/100;
-    e=a%10000%1000%100/10;
-    f=a%10000%1000%100%10/1;
-    if(b==0)
+    char a[15];
+    int i;
+    scanf("%s",&a);
+    for(i=0;i<a[i];i++)
     {
-        printf("Zero");
+        switch(a[i])
+        {
+            case '0': printf("Zero"); break;
+            case '1': printf("One"); break;
+            case '2': printf("Two"); break;
+            case '3': printf("Three"); break;
+            case '4': printf("Four"); break;
+            case '5': printf("Five"); break;
+            case '6': printf("Six"); break;
+            case '7': printf("Seven"); break;
+            case '8': printf("Eight"); break;
+            case '9': printf("Nine"); break;
+        }
+        if(i<a[i+1])
+        {
+            printf("-");
+        }
     }
-    else if(b==1)
-    {
-        printf("One");
-    }
-    else if(b==2)
-    {
-        printf("Two");
-    }
-    else if(b==3)
-    {
-        printf("Three");
-    }
-    else if(b==4)
-    {
-        printf("Four");
-    }
-    else if(b==5)
-    {
-        printf("Five");
-    }
-    else if(b==6)
-    {
-        printf("Six");
-    }
-    else if(b==7)
-    {
-        printf("Seven");
-    }
-    else if(b==8)
-    {
-        printf("Eight");
-    }
-    else
-    {
-        printf("Nine");
-    }
-    printf("-");
-    if(c==0)
-    {
-        printf("Zero");
-    }
-    else if(c==1)
-    {
-        printf("One");
-    }
-    else if(c==2)
-    {
-        printf("Two");
-    }
-    else if(c==3)
-    {
-        printf("Three");
-    }
-    else if(c==4)
-    {
-        printf("Four");
-    }
-    else if(c==5)
-    {
-        printf("Five");
-    }
-    else if(c==6)
-    {
-        printf("Six");
-    }
-    else if(c==7)
-    {
-        printf("Seven");
-    }
-    else if(c==8)
-    {
-        printf("Eight");
-    }
-    else
-    {
-        printf("Nine");
-    }
-    printf("-");
-    if(d==0)
-    {
-        printf("Zero");
-    }
-    else if(d==1)
-    {
-        printf("One");
-    }
-    else if(d==2)
-    {
-        printf("Two");
-    }
-    else if(d==3)
-    {
-        printf("Three");
-    }
-    else if(d==4)
-    {
-        printf("Four");
-    }
-    else if(d==5)
-    {
-        printf("Five");
-    }
-    else if(d==6)
-    {
-        printf("Six");
-    }
-    else if(d==7)
-    {
-        printf("Seven");
-    }
-    else if(d==8)
-    {
-        printf("Eight");
-    }
-    else
-    {
-        printf("Nine");
-    }
-
-    printf("-");
-    if(e==0)
-    {
-        printf("Zero");
-    }
-    else if(e==1)
-    {
-        printf("One");
-    }
-    else if(e==2)
-    {
-        printf("Two");
-    }
-    else if(e==3)
-    {
-        printf("Three");
-    }
-    else if(e==4)
-    {
-        printf("Four");
-    }
-    else if(e==5)
-    {
-        printf("Five");
-    }
-    else if(e==6)
-    {
-        printf("Six");
-    }
-    else if(e==7)
-    {
-        printf("Seven");
-    }
-    else if(e==8)
-    {
-        printf("Eight");
-    }
-    else
-    {
-        printf("Nine");
-    }
-
-    printf("-");
-    if(f==0)
-    {
-        printf("Zero");
-    }
-    else if(f==1)
-    {
-        printf("One");
-    }
-    else if(f==2)
-    {
-        printf("Two");
-    }
-    else if(f==3)
-    {
-        printf("Three");
-    }
-    else if(f==4)
-    {
-        printf("Four");
-    }
-    else if(f==5)
-    {
-        printf("Five");
-    }
-    else if(f==6)
-    {
-        printf("Six");
-    }
-    else if(f==7)
-    {
-        printf("Seven");
-    }
-    else if(f==8)
-    {
-        printf("Eight");
-    }
-    else
-    {
-        printf("Nine");
-    }
-
-
     return 0;
 }
