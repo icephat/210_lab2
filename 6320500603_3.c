e6b269ba24ed4086d83e98fa285ace255a25ce3a
icephat <phattaraporn.sa@ku.th>
2021-02-11 17:50:03 +0700

    แก้ไขใหม่
    คิดโค้ดใหม่ทั้งหมด

--- 6320500603_3.c
+++ 6320500603_3.c
@@ -1,27 +1,51 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-   int a,b,c,x,y,z;
-   char aa,bb,cc;
-   scanf("%d %d %d",&a,&b,&c);
-   scanf("%d%d%d",&aa,&bb,&cc);
-   if(a<b&&a<c)
-   {
-       x=a;
-       if(x==aa)
-       {
-           printf("%d",a);
-       }
+    int a[3]={},i,mi,j,x,b,c;
+    char y[3]={};
 
-   }
-   if(b>a&&b<c)
-   {
-       y=b;
-   }
-   if(c>a&&c>b)
-   {
-       z=c;
-   }
+    for(i=0;i<3;i++)
+    {
+        scanf("%d",&a[i]);
+    }
 
+    for(i=0;i<3-1;i++)
+    {
+        for(j=0;j<3-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                mi = a[j+1];
+                a[j+1] = a[j];
+                a[j] = mi;
+            }
+        }
+    }
+    x = a[0];
+    b = a[1];
+    c = a[2];
+
+    scanf("%s",y);
+
+    for(i=0;i<3;i++)
+    {
+        if(y[i]=='A')
+        {
+            a[i] = x;
+        }
+        else if(y[i]=='B')
+        {
+            a[i] = b;
+        }
+        else if(y[i]=='C')
+        {
+            a[i]= c;
+        }
+    }
+    for(i=0;i<3;i++)
+    {
+        printf("%d ",a[i]);
+    }
     return 0;
+
 }
